452959f90e6384af9af7d8347ec4e8c17bba0405
kanlar75 <kanlar75@gmail.com>
2023-11-02 19:35:09 +0300

    course7

---
+++ c7/users/validators.py
@@ -0,0 +1,16 @@
+from rest_framework.exceptions import ValidationError
+
+from users.models import User
+
+
+class ChatIdValidator:
+    """ "Проверка ввода chat_id """
+
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        chat_id = dict(value).get(self.field)
+        if not chat_id:
+            print('chat_id нужен для отправки уведомлений')
+        return True
